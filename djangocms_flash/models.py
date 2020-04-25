12b4b320c62b8d09eb2af1940afa7da573b5b01b
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2020-04-25 22:25:27 +0300

    Fix import python_2_unicode_compatible

--- djangocms_flash/models.py
+++ djangocms_flash/models.py
@@ -14,7 +14,10 @@ except ImportError:
         for information
         """
         return instance.get_media_path(filename)
-from cms.utils.compat.dj import python_2_unicode_compatible
+try:
+    from django.utils.encoding import python_2_unicode_compatible
+except ImportError:
+    from six import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
